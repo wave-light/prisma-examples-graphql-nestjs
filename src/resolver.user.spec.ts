00e4284be3cce0fcc6992a936c6a4f5006de099a
wave-light <40989533+wave-light@users.noreply.github.com>
2022-04-21 17:04:25 +0100

    Typescript error demo

--- src/resolver.user.spec.ts
+++ src/resolver.user.spec.ts
@@ -1,8 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
 import { UserResolver } from './resolvers.user';
+import { PrismaService } from './prisma.service';
+import { User } from '@prisma/client';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
+  const prismaMock = createMock<PrismaService>();
+  prismaMock.user.findMany.mockResolvedValue({ id: 1 } as User); // <---- TS validation error - Property 'mockResolvedValue' does not exist on type
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
